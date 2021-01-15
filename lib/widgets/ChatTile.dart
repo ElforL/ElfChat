1d3013dfea484db9bf52f491a436cc971468bf8b
Elfor <lshono8@gmail.com>
2021-01-15 05:28:05 +0300

    📷 added image icon if last message has one

--- lib/widgets/ChatTile.dart
+++ lib/widgets/ChatTile.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elfchat/models/Chat.dart';
-import 'package:elfchat/models/User.dart';
-import 'package:elfchat/screens/ChatPage.dart';
 import 'package:elfchat/services/FireStoreServices.dart';
 import 'package:elfchat/services/auth.dart';
 import 'package:flutter/material.dart';
@@ -36,7 +34,24 @@ class _ChatTileState extends State<ChatTile> {
             stream: widget.db.getLastMessage(widget.elfChat.chatID),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.active && snapshot.data.docs.length > 0) {
-                return Text((snapshot.data as QuerySnapshot).docs.first.data()['message']);
+                var msgText = (snapshot.data as QuerySnapshot).docs.first.data()['message'];
+                var hasPhoto = (snapshot.data as QuerySnapshot).docs.first.data()['attachmentURL'] != null;
+                return Row(
+                  children: [
+                    if (hasPhoto)
+                      Icon(
+                        Icons.image,
+                        size: 17,
+                        color: Colors.grey[700],
+                      ),
+                    Text(
+                      msgText,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                      maxLines: 1,
+                    ),
+                  ],
+                );
               }
               return SizedBox();
             },
